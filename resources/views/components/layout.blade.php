6cb8a8f59928b36b9515648915386ed4be7aaeec
Wediah <100675006+Wediah@users.noreply.github.com>
2024-08-12 10:28:10 +0000

    removes budgetting

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -201,12 +201,12 @@
                                 <span class="flex-1 ms-3 whitespace-nowrap ml-2">Analytics</span>
                             </a>
                         </li>
-                        <li>
-                            <a href="{{ route('budget.create') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
-                                <i class='bx bx-bar-chart'></i>
-                                <span class="flex-1 ms-3 whitespace-nowrap ml-2">Budget</span>
-                            </a>
-                        </li>
+{{--                        <li>--}}
+{{--                            <a href="{{ route('budget.create') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">--}}
+{{--                                <i class='bx bx-bar-chart'></i>--}}
+{{--                                <span class="flex-1 ms-3 whitespace-nowrap ml-2">Budget</span>--}}
+{{--                            </a>--}}
+{{--                        </li>--}}
                         <li>
                             <a href="{{ route('category.create') }}" class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                                 <i class='bx bx-category'></i>
